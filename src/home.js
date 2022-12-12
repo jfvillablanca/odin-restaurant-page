39cb576eea2ff4268aaeb3406781b10b5df38805
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-13 07:41:23 +0800

    Move homepage from index.js to home.js as modules

---
+++ src/home.js
@@ -0,0 +1,49 @@
+import ImgHero from './images/assets/alexandru-bogdan-ghita-UeYkqQh4PoI-unsplash.jpg';
+
+const divHero = document.createElement("div");
+divHero.classList.add("hero");
+const h1Welcome = document.createElement("h1");
+h1Welcome.textContent = "The Jorrvaskr";
+const h2Welcome = document.createElement("h2");
+h2Welcome.textContent = "Let's feast in Valhalla";
+const imgHero = document.createElement("img");
+imgHero.setAttribute("src", ImgHero);
+imgHero.setAttribute("alt", "A lot of meat on the grill");
+
+const headerWelcome = document.createElement("div");
+headerWelcome.classList.add("headers");
+headerWelcome.appendChild(h1Welcome);
+headerWelcome.appendChild(h2Welcome);
+divHero.appendChild(headerWelcome);
+divHero.appendChild(imgHero);
+
+const navTabs = document.createElement("nav");
+navTabs.classList.add("tabs");
+const ulTabs = document.createElement("ul");
+
+const liTabHome = document.createElement("li");
+const aTabHome = document.createElement("a");
+aTabHome.setAttribute("href", "#");
+aTabHome.textContent = "Home";
+aTabHome.classList.add("current-tab");
+liTabHome.appendChild(aTabHome);
+
+const liTabMenu = document.createElement("li");
+const aTabMenu = document.createElement("a");
+aTabMenu.setAttribute("href", "#");
+aTabMenu.textContent = "Menu";
+liTabMenu.appendChild(aTabMenu);
+
+const liTabContact = document.createElement("li");
+const aTabContact = document.createElement("a");
+aTabContact.setAttribute("href", "#");
+aTabContact.textContent = "Contact";
+liTabContact.appendChild(aTabContact);
+
+ulTabs.appendChild(liTabHome);
+ulTabs.appendChild(liTabMenu);
+ulTabs.appendChild(liTabContact);
+
+navTabs.appendChild(ulTabs);
+
+export {navTabs, divHero, aTabHome, aTabMenu, aTabContact};
--- src/index.js
+++ src/index.js
@@ -2,54 +2,13 @@ import './css/normalize.css';
 import './css/skeleton.css';
 import './css/styles.css';
 
-import ImgHero from './images/assets/alexandru-bogdan-ghita-UeYkqQh4PoI-unsplash.jpg';
+import {navTabs, divHero, aTabHome, aTabMenu, aTabContact} from './home.js';
 
 const containerContent = document.querySelector("#content");
 
-const divHero = document.createElement("div");
-divHero.classList.add("hero");
-const h1Welcome = document.createElement("h1");
-h1Welcome.textContent = "The Jorrvaskr";
-const h2Welcome = document.createElement("h2");
-h2Welcome.textContent = "Let's feast in Valhalla";
-const imgHero = document.createElement("img");
-imgHero.setAttribute("src", ImgHero);
-imgHero.setAttribute("alt", "A lot of meat on the grill");
+aTabHome.addEventListener("click", () => {
+  console.log("Home");
+})
 
-const headerWelcome = document.createElement("div");
-headerWelcome.classList.add("headers");
-headerWelcome.appendChild(h1Welcome);
-headerWelcome.appendChild(h2Welcome);
-divHero.appendChild(headerWelcome);
-divHero.appendChild(imgHero);
-
-const navTabs = document.createElement("nav");
-navTabs.classList.add("tabs");
-const ulTabs = document.createElement("ul");
-
-const liTabHome = document.createElement("li");
-const aTabHome = document.createElement("a");
-aTabHome.setAttribute("href", "#");
-aTabHome.textContent = "Home";
-aTabHome.classList.add("current-tab");
-liTabHome.appendChild(aTabHome);
-
-const liTabMenu = document.createElement("li");
-const aTabMenu = document.createElement("a");
-aTabMenu.setAttribute("href", "#");
-aTabMenu.textContent = "Menu";
-liTabMenu.appendChild(aTabMenu);
-
-const liTabContact = document.createElement("li");
-const aTabContact = document.createElement("a");
-aTabContact.setAttribute("href", "#");
-aTabContact.textContent = "Contact";
-liTabContact.appendChild(aTabContact);
-
-ulTabs.appendChild(liTabHome);
-ulTabs.appendChild(liTabMenu);
-ulTabs.appendChild(liTabContact);
-
-navTabs.appendChild(ulTabs);
 containerContent.appendChild(navTabs);
 containerContent.appendChild(divHero);
