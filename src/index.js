9f1e96e45629b6f368c5fdd2814e58478bfb51c0
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-13 07:55:00 +0800

    Import navbar and add event listeners to navbar elements

--- src/index.js
+++ src/index.js
@@ -2,13 +2,32 @@ import './css/normalize.css';
 import './css/skeleton.css';
 import './css/styles.css';
 
-import {navTabs, divHero, aTabHome, aTabMenu, aTabContact} from './home.js';
+import {navTabs, aTabHome, aTabMenu, aTabContact} from './navbar.js';
+import {divHero as divHome} from './home.js';
 
 const containerContent = document.querySelector("#content");
 
 aTabHome.addEventListener("click", () => {
   console.log("Home");
+  aTabHome.classList.add("current-tab");
+  aTabMenu.classList.remove("current-tab");
+  aTabContact.classList.remove("current-tab");
+})
+
+aTabMenu.addEventListener("click", () => {
+  console.log("Menu");
+  aTabHome.classList.remove("current-tab");
+  aTabMenu.classList.add("current-tab");
+  aTabContact.classList.remove("current-tab");
+})
+
+
+aTabContact.addEventListener("click", () => {
+  console.log("Contact");
+  aTabHome.classList.remove("current-tab");
+  aTabMenu.classList.remove("current-tab");
+  aTabContact.classList.add("current-tab");
 })
 
 containerContent.appendChild(navTabs);
-containerContent.appendChild(divHero);
+containerContent.appendChild(divHome);
