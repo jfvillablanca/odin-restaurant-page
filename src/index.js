1b7992362876c8dac521133068b3ef25d251e34d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-12 12:20:29 +0800

    Implement basic html struc in index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,37 @@ import './css/normalize.css';
 import './css/skeleton.css';
 import './css/styles.css';
 
-console.log("Hello world");
+const containerContent = document.querySelector("#content");
+
+const divHero = document.createElement("div");
+divHero.classList.add("hero");
+const h1Welcome = document.createElement("h1");
+h1Welcome.textContent = "Welcome to Jorrvaskr";
+const h2Welcome = document.createElement("h2");
+h2Welcome.textContent = "Where we feast like we're in Valhalla";
+const imgHero = document.createElement("img");
+imgHero.setAttribute("src", "");
+imgHero.setAttribute("alt", "A lot of meat on the grill");
+
+divHero.appendChild(h1Welcome);
+divHero.appendChild(h2Welcome);
+divHero.appendChild(imgHero);
+
+const divTabs = document.createElement("div");
+divTabs.classList.add("tabs");
+const divTabHome = document.createElement("div");
+divTabHome.classList.add("home");
+divTabHome.textContent = "Home";
+const divTabMenu = document.createElement("div");
+divTabMenu.classList.add("menu");
+divTabMenu.textContent = "Menu";
+const divTabContact = document.createElement("div");
+divTabContact.classList.add("contact");
+divTabContact.textContent = "Contact";
+
+divTabs.appendChild(divTabHome);
+divTabs.appendChild(divTabMenu);
+divTabs.appendChild(divTabContact);
+
+containerContent.appendChild(divHero);
+containerContent.appendChild(divTabs);
