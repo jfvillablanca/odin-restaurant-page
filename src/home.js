fae73e050698dd149f1018d07b1033224f1ab7e3
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-13 09:06:21 +0800

    Rename exported div

--- src/home.js
+++ src/home.js
@@ -1,7 +1,8 @@
 import ImgHero from './images/assets/alexandru-bogdan-ghita-UeYkqQh4PoI-unsplash.jpg';
 
-const divHero = document.createElement("div");
-divHero.classList.add("hero");
+const divHome = document.createElement("div");
+divHome.classList.add("home");
+divHome.classList.add("page");
 const h1Welcome = document.createElement("h1");
 h1Welcome.textContent = "The Jorrvaskr";
 const h2Welcome = document.createElement("h2");
@@ -14,7 +15,7 @@ const headerWelcome = document.createElement("div");
 headerWelcome.classList.add("headers");
 headerWelcome.appendChild(h1Welcome);
 headerWelcome.appendChild(h2Welcome);
-divHero.appendChild(headerWelcome);
-divHero.appendChild(imgHero);
+divHome.appendChild(headerWelcome);
+divHome.appendChild(imgHero);
 
-export {divHero};
+export {divHome};
