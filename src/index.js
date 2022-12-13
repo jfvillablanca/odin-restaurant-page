a96ebe10092943e4605e543ba8712286d4d3bd84
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-13 09:24:09 +0800

    Add func for changing page on tab click

--- src/index.js
+++ src/index.js
@@ -3,31 +3,47 @@ import './css/skeleton.css';
 import './css/styles.css';
 
 import {navTabs, aTabHome, aTabMenu, aTabContact} from './navbar.js';
-import {divHero as divHome} from './home.js';
+import {divHome} from './home.js';
+import {divMenu} from './menu.js';
+import {divContact} from './contact.js';
 
 const containerContent = document.querySelector("#content");
+containerContent.appendChild(navTabs);
+divHome.classList.add("current-tab");
+containerContent.appendChild(divHome);
+containerContent.appendChild(divMenu);
+containerContent.appendChild(divContact);
 
 aTabHome.addEventListener("click", () => {
-  console.log("Home");
   aTabHome.classList.add("current-tab");
+  divHome.classList.add("current-tab");
+
   aTabMenu.classList.remove("current-tab");
+  divMenu.classList.remove("current-tab");
+
   aTabContact.classList.remove("current-tab");
+  divContact.classList.remove("current-tab");
 })
 
 aTabMenu.addEventListener("click", () => {
-  console.log("Menu");
   aTabHome.classList.remove("current-tab");
+  divHome.classList.remove("current-tab");
+
   aTabMenu.classList.add("current-tab");
+  divMenu.classList.add("current-tab");
+
   aTabContact.classList.remove("current-tab");
+  divContact.classList.remove("current-tab");
 })
 
 
 aTabContact.addEventListener("click", () => {
-  console.log("Contact");
   aTabHome.classList.remove("current-tab");
-  aTabMenu.classList.remove("current-tab");
-  aTabContact.classList.add("current-tab");
-})
+  divHome.classList.remove("current-tab");
 
-containerContent.appendChild(navTabs);
-containerContent.appendChild(divHome);
+  aTabMenu.classList.remove("current-tab");
+  divMenu.classList.remove("current-tab");
+
+  aTabContact.classList.add("current-tab");
+  divContact.classList.add("current-tab");
+})
