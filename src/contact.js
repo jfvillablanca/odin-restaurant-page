4652ef1eba6eb1522cc93c633a9b9c6f56928d0a
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-14 17:06:55 +0800

    Import image assets and format code

--- src/contact.js
+++ src/contact.js
@@ -1,21 +1,24 @@
-import ImgBg from './images/assets/volkan-vardar-1H30uRC1plc-unsplash.jpg';
+import './css/contact.css';
+import ImgBg from './images/assets/nick-karvounis-Ciqxn7FE4vE-unsplash.jpg';
+import ImgLoc1 from './images/assets/volkan-vardar-1H30uRC1plc-unsplash.jpg';
+import ImgLoc2 from './images/assets/adrien-olichon-ZgREXhl8ER0-unsplash.jpg';
 
 const divContact = document.createElement("div");
 divContact.classList.add("contact");
 divContact.classList.add("page");
+
 const h1Welcome = document.createElement("h1");
-h1Welcome.textContent = "The Jorrvaskr";
-const h2Welcome = document.createElement("h2");
-h2Welcome.textContent = "Let's feast in Valhalla";
-const imgBackground = document.createElement("img");
-imgBackground.setAttribute("src", ImgBg);
-imgBackground.setAttribute("alt", "A lot of meat on the grill");
-imgBackground.classList.add("background-image");
+h1Welcome.textContent = "Book a reservation";
 
 const headerWelcome = document.createElement("div");
-headerWelcome.classList.add("headers");
+headerWelcome.classList.add("header");
 headerWelcome.appendChild(h1Welcome);
-headerWelcome.appendChild(h2Welcome);
+
+const imgBackground = document.createElement("img");
+imgBackground.setAttribute("src", ImgBg);
+imgBackground.setAttribute("alt", "A picture of a rustic restaurant");
+imgBackground.classList.add("background-image");
+
 divContact.appendChild(imgBackground);
 divContact.appendChild(headerWelcome);
 
